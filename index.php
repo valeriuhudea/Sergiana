2075d6173e883ff6ac5ad63a61e2066daff6bab8
Florin-Corcaci <31889593+Florin-Corcaci@users.noreply.github.com>
2017-10-16 18:12:53 +0300

    Prima pagina

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title> titlu</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>TextTextTextText</h1>
+
+	<ul>
+		<li><a href="create.php"><strong>Creaza</strong></a> - Adauga o fisa</li>
+		<li><a href="read.php"><strong>Cauta</strong></a> - Cauta o fisa</li>
+	</ul>
+
+</body>
+</html>
