296d517e011ccd5eef5b1d6a61fb162f3e8ad0e9
Florin-Corcaci <31889593+Florin-Corcaci@users.noreply.github.com>
2017-10-16 18:23:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<!doctype html>
-<html lang="en">
+<?php include "templates/header.php"; ?>
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<ul>
+	<li><a href="create.php"><strong>Creaza</strong></a> - Adauga o fisa</li>
+	<li><a href="read.php"><strong>Cauta</strong></a> - Cauta o fisa</li>
+</ul>
 
-	<title> titlu</title>
-
-	<link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-	<h1>TextTextTextText</h1>
-
-	<ul>
-		<li><a href="create.php"><strong>Creaza</strong></a> - Adauga o fisa</li>
-		<li><a href="read.php"><strong>Cauta</strong></a> - Cauta o fisa</li>
-	</ul>
-
-</body>
-</html>
+<?php include "templates/footer.php"; ?>
