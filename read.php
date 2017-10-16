d6e0ed219f252e13e1e00661540d7353e2aac1da
Florin <31889593+Florin-Corcaci@users.noreply.github.com>
2017-10-16 19:09:48 +0300

    Update read.php

--- read.php
+++ read.php
@@ -1,2 +1,13 @@
 <?php include "templates/header.php"; ?>
+
+<h2>Cauta Fise dupa Locatia interventiei</h2>
+
+<form method="post">
+	<label for="location">Locatia interventiei</label>
+	<input type="text" id="location" name="location">
+	<input type="submit" name="submit" value="View Results">
+</form>
+
+<a href="index.php">Inapoi la prima pagina</a>
+
 <?php include "templates/footer.php"; ?>
